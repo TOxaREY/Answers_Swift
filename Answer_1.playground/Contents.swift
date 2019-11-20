f68509f522c24cc3de175f6104fe8cda7ce9fba8
TOxaREY <delta@toxarey.xyz>
2019-11-20 19:19:37 +0300

    answer 1

---
+++ Answer_1.playground/Contents.swift
@@ -0,0 +1,36 @@
+import UIKit
+import PlaygroundSupport
+
+protocol Audible {
+    func makeSound()
+}
+//Расширения протоколов могут добавлять реализацию к соответствующим типам данных, но не могут расширить протокол или унаследовать от другого протокола. Наследование протокола всегда указывается в самом объявлении протокола. Вы можете использовать расширение протокола, чтобы обеспечить реализацию по умолчанию для любого метода или требования свойства этого протокола.
+extension Audible {
+    func makeSound() {
+        print("*abstract sound*")
+    }
+}
+struct Cat {}
+//Расширение может расширить существующий тип для того, чтобы он соответствовал одному или более протоколам. Там где это имеет место, имена протоколов записываются точно так же, как и в случае с классами или структурами:
+/* extension SomeType: SomeProtocol, AnotherProtocol {
+        реализация требования протокола тут
+    } */
+extension Cat: Audible {
+    func makeSound() {
+        print("Meaow!")
+    }
+}
+//Аргумент функции speak подписан под протокол Audible, значит будет вызываться всё подписанное под этот протокол.
+func speak(_ target: Audible) {
+    target.makeSound()
+}
+//Структура Cat уже подписанна протоколом, поэтому можно и так let audible = Cat(), но объявление типа повысит надежность.
+let audible: Audible = Cat()
+speak(audible)
+
+// -    You cannot extend the Cat struct to add a protocol conformance in this way.
+// -    You cannot extend a protocol to add a default makeSound implementation in this way.
+// -    It's undefined whether Audible.makeSound or Cat.makeSound will be called in target.makeSound().
+// -    You cannot use the protocol as a type name in let audible: Audible = Cat().
+// -    The code will print *abstract sound* when run.
+// +    The code will print Meaow! when run.
---
+++ README.md
@@ -0,0 +1,21 @@
+# Answers Swift
+#### ноябрь 2019
+---
+##### Answer 1
+What statements about the following code are true?
+
+![](https://github.com/TOxaREY/Answers_Swift/blob/master/image/Answer_1.png)
+□ You cannot extend the  _Cat_  struct to add a protocol conformance in this way.
+
+□ You cannot extend a protocol to add a default  _makeSound_  implementation in this way.
+
+□ It's undefined whether Audible.makeSound or Cat.makeSound will be called in target.makeSound().
+
+□ You cannot use the protocol as a type name in  _let audible: Audible = Cat()._
+
+□ The code will print  _*abstract sound_* when run.
+
+□ The code will print  _Meaow!_  when run.
+[Answer 1](https://github.com/TOxaREY/Answers_Swift/blob/master/Answer_1.playground)
+
+---
