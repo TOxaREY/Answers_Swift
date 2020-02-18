78faf7cda2f00a71a88ee90bb922a8904e21572e
TOxaREY <delta@toxarey.xyz>
2020-02-18 15:00:54 +0300

    update qa

---
+++ qa/QA_14.playground/Contents.swift
@@ -0,0 +1,55 @@
+//: A UIKit based Playground for presenting user interface
+
+import UIKit
+import PlaygroundSupport
+
+class MyViewController : UIViewController {
+    override func loadView() {
+        let view = UIView()
+        view.backgroundColor = .white
+        self.view = view
+        
+        func updateProgressBar() {
+            var boxFrame = CGRect(x: 10, y: 10, width: 35, height: 50)
+            for _ in 0..<10 {
+                let box = UIView(frame: boxFrame)
+                box.backgroundColor = .blue
+                view.addSubview(box)
+                
+                // What goes here?
+                view.setNeedsDisplay()
+                Thread.sleep(forTimeInterval: 2)
+                boxFrame.origin.x += boxFrame.width
+                print("qqq")
+            }
+        }
+        updateProgressBar()
+    }
+}
+
+// Present the view controller in the Live View window
+PlaygroundPage.current.liveView = MyViewController()
+
+
+/*
+ draw(_:) - рисует изображение получателя в переданном прямоугольнике.
+ UIGraphicsGetCurrentContext - возвращает текущий графический контекст.
+ setNeedsDisplay() - помечает весь прямоугольник границ
+ получателя как подлежащий перерисовке.
+ sleep(forTimeInterval:) - обработка цикла выполнения не происходит,
+ пока поток заблокирован. Пользовательский интерфейс не отвечает в течение этого времени(плохо).
+ */
+
+
+
+/*
+ ○   draw(bounds)
+ 
+ ○   layer.draw(in: UIGraphicsGetCurrentContext()!)
+ 
+ ○   setNeedsDisplay()
+ 
+ ●   Этот подход неверен. Это должно быть реализовано другим способом,
+ например, с DispatchQueue.asyncAfter.
+ */
+
